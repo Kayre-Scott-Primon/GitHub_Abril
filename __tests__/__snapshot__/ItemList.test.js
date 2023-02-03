256dd532a147490ec9ff23d2b1d66af3ebd8441a
Kayré Scott Primon <64801417+Kayre-Scott-Primon@users.noreply.github.com>
2023-02-03 03:05:10 -0300

    test: implemment snapshop tests

---
+++ __tests__/__snapshot__/ItemList.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+import ItemRepo from '../../src/components/ItemRepo'
+import { Provider } from "react-redux";
+import store from "../../src/storage/redux";
+
+const mockedDispatch = jest.fn();
+
+jest.mock("@react-navigation/native", () => {
+    const actualNav = jest.requireActual("@react-navigation/native");
+    return {
+        ...actualNav,
+        useNavigation: () => ({
+        navigate: jest.fn(),
+        dispatch: mockedDispatch,
+        }),
+    };
+});
+
+describe('ItemRepo', () => {
+
+  // genarate snapshot file
+    /*it('renders correctly when there are no items', () => {
+      const tree = renderer.create(            
+        <Provider store={store}>
+          <ItemRepo
+              data={{
+                  name: 'name',
+                  url: 'url',
+                  owner: {
+                      login: 'user'
+                  },
+                  stargazers_count: 0
+              }}
+          />
+        </Provider>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });*/
+
+    it('renders correctly when there is a single item', () => {
+      const tree = renderer.create(
+        <Provider store={store}>
+          <ItemRepo
+              data={{
+                  name: 'name',
+                  url: 'url',
+                  owner: {
+                      login: 'user'
+                  },
+                  stargazers_count: 0
+              }}
+          />
+        </Provider>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+})
